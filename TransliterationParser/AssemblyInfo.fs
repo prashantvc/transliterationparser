f333284e5095904cdb42298572bdb71e41bd74e7
Prashant Cholachagudda <pvc@outlook.com>
2016-11-18 21:28:50 +0530

    initial commit

---
+++ TransliterationParser/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace TransliterationParser
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("TransliterationParser")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("prashant")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ TransliterationParser/Program.fs
@@ -0,0 +1,17 @@
+﻿open FSharp.Data
+open System.Text.RegularExpressions
+
+type InputMethod = XmlProvider <"transliteration.xml", Global = true>
+
+[<EntryPoint>]
+let main argv = 
+    let content = InputMethod.GetSample()
+    let mutable text = ""
+
+    for ch in argv.[0] do
+        text <- text + ch.ToString()
+        let input = content.Patterns |> Seq.where(fun a -> Regex.IsMatch(text, a.Input.Value)) |> Seq.head
+        text <- Regex.Replace(text, input.Input.Value, input.Replacement.Value)
+    
+    printfn "%s - %s" argv.[0] text
+    0
